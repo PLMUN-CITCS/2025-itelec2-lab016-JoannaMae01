e18e0589adbdab57008a4e15013181d9548faca7
JoannaMae01 <arciagajoannamae_bsit@plmun.edu.ph>
2025-02-19 16:16:31 +0800

    Submitting Python Week 04 - Laboratory # 16

--- basic_function.py
+++ basic_function.py
@@ -1,5 +1,8 @@
+# Define the function using def
 def greet():
-def greet():
-    """Prints a greeting message."""  # Docstring
-    print("Hello, Python Students!")  # Function body
+    """Prints a greeting message."""
+    # Function body: printing the greeting message
+    print("Hello, Python Students!")  # This is the task the function performs
+
+# Call the function
 greet()
